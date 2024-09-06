eef05a964e535344d191a9d8be3472465c607c1e
kukidon820 <kukidon820@gmail.com>
2024-09-06 13:10:04 +0300

    Update main.js

--- laba_1/main.js
+++ laba_1/main.js
@@ -1,10 +1,8 @@
-
 let numberOfFilms;
 
 do {
-    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+    numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '').trim();
 } while (numberOfFilms === '' || numberOfFilms == null || isNaN(numberOfFilms));
-
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -15,16 +13,14 @@ for (let i = 0; i < 2; i++) {
     let lastMovie, movieRating;
 
     do {
-        lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+        lastMovie = prompt('Один из последних просмотренных фильмов?', '').trim();
     } while (lastMovie === '' || lastMovie == null || lastMovie.length > 50);
 
     do {
-        movieRating = prompt('На сколько оцените его?', '');
+        movieRating = prompt('На сколько оцените его?', '').trim();
     } while (movieRating === '' || movieRating == null || isNaN(movieRating));
-
 
     personalMovieDB.movies[lastMovie] = movieRating;
 }
-
 
 document.body.innerHTML = `<pre>${JSON.stringify(personalMovieDB, null, 2)}</pre>`;
